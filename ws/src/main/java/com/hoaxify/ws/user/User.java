3e3270ffdb17ba877f725db82220e398c702bc3f
N2rmin <t.n2rmin@gmail.com>
2022-12-06 16:58:09 -0800

    CORS

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,2 +1,12 @@
-package com.hoaxify.ws.user;public class User {
+package com.hoaxify.ws.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String username;
+    private String displayName;
+    private String password;
+
+
 }
