ee1df1207b51e74a7338dc190e197c24db490526
N2rmin <t.n2rmin@gmail.com>
2022-12-12 17:09:34 -0800

    validation

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,11 +1,17 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.user.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -20,8 +26,28 @@ public class UserController {
     @CrossOrigin
     @PostMapping("/api/1.0/users")
     @ResponseStatus(HttpStatus.CREATED)
-    public GenericResponse createUser(@RequestBody User user){
+    public ResponseEntity<?> createUser(@RequestBody User user){
+        ApiError error= new ApiError(400,"Validation error","/api/1.0/users");
+        Map<String,String> validationErrors= new HashMap<>();
+
+        String username= user.getUsername();
+        String displayName= user.getDisplayName();
+
+        if (username==null || username.isEmpty()){
+
+            validationErrors.put("username","Username cannot be null");
+
+        }
+
+        if (displayName==null || displayName.isEmpty()){
+
+            validationErrors.put("displayName","DisplayName cannot be null");
+
+        }if(validationErrors.size()>0){
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
         userService.save(user);
-        return new GenericResponse("user created");
+        return ResponseEntity.ok( new GenericResponse("user created"));
     }
 }
